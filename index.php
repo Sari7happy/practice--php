31553cc23ee0be5d0b6bb330263ef0627eba0518
sari7happy <sari7happy@gmail.com>
2022-09-29 00:05:21 +0900

    ”php問い合わせフォーム”

--- index.php
+++ index.php
@@ -1,3 +1,91 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors','On');
+
+if(!empty($_POST)){
+    define('MSG01','入力必須です。');
+    define('MSG02','Emailの形式で入力してください。');
+    define('MSG03','パスワード（再入力）が合っていません。');
+    define('MSG04','半角英数字のみご利用ください。');
+    define('MSG05','6文字以上で入力してください。');
+    
+    $err_flg =false;
+    $err_msg =array();
+
+if(empty($POST['email'])){
+    $err_msg['email'] = MSG01;
+}
+if(empty($_POST['pass'])){
+    $err_msg['pass'] = MSG01;
+}
+if(empty($_POST['pass_retype'])){
+    $err_msg['pass_retype'] = MSG01;
+}
+if(empty($err_msg)){
+    $email =$_POST['email'];
+    $pass =$_POST['pass'];
+    $pass_re =$_POST['pass_retype'];
+
+
+}
+
+if(!preg_match("/^([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+$/".$email)){
+    $err_msg['email'] = MSG02;
+}
+if($pass)! == $pass_re){
+    $err_msg['pass'] = MSG03;
+}
+if(empty($err_msg)){
+    if(!preg_match("/^[a-zA-Z0-9])+$/".$pass)){
+        $err_msg['pass'] =MSG04;
+    }else if(mb_strlen($pass) < 6){
+        $err_msg['pass'] = MSG05;
+    }
+if(empty($err_msg)) header("location:mypage.php");
+}
+
+}
+
+1
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 <!DOCTYPE html>
 <html>
 <head>
