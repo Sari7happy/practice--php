23cac9ece4256e13321421ce03b66640355d7448
sari7happy <sari7happy@gmail.com>
2022-09-30 20:46:02 +0900

    ”お問合せフォーム2”

--- mail.php
+++ mail.php
@@ -1,49 +1,28 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors','On');
+if(!empty($to) && !empty($subject) && !empty($comment)){
+mb_language("japanese");
+mb_internal_encoding("UTF-8");
 
-session_start();
 
-if(empty($_SESSION['login'])) header("Location:login.php");
+
+$from ='info@webukatu.com';
+
+
+$result =mb_send_mail($to,$subject,$comment,"From:".$from);
+
+
+if($result){
+    unset($_POST);
+    $msg ='メッセージが送信されました。';
+
+}else{
+    $msg ='メッセージの送信に失敗しました。';
+}
+
+}else{
+    $msg ='全て入力必須です。';
+
+}
+
+
 ?>
-
-
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <title>ホームページのタイトル</title>
-    <style>
-        body{
-            margin: 0 auto;
-            padding: 150px;
-            width: 25%;
-            background: #fbfbfa;
-        }
-        h1{ color: #545454; font-size: 20px;}
-        a{
-            color: #545454;
-            display: block;
-        }
-        a:hover{
-            text-decoration: none;
-        }
-    </style>
-</head>
-<body>
-        <?php if(!empty($_SESSION['login'])){ ?>
-        <h1>マイページ</h1>
-            <section>
-                <p>
-                    あなたのemailは info@webukatu.com です。<br />
-                    あなたのpassは password です。
-                </p>
-                <a href="index.php">ユーザー登録画面へ</a>
-
-            </section>
-            <?php } else { ?>
-                <p>ログインしてないと見れません。</p>
-            <?php } ?>
-
-</body>
-</html>
